3a8def76c029047d836dd1d43749d50f4d074dd8
Christian D. Ott <christian.d.ott@gmail.com>
2018-11-11 19:22:27 -0800

    add script to pull raw data

---
+++ data/get_data.sh
@@ -0,0 +1,8 @@
+wget https://s3.amazonaws.com/tripdata/201307-citibike-tripdata.csv.zip
+wget https://s3.amazonaws.com/tripdata/201407-citibike-tripdata.csv.zip
+wget https://s3.amazonaws.com/tripdata/201507-citibike-tripdata.csv.zip
+wget https://s3.amazonaws.com/tripdata/201607-citibike-tripdata.csv.zip
+wget https://s3.amazonaws.com/tripdata/201707-citibike-tripdata.csv.zip
+wget https://s3.amazonaws.com/tripdata/201807-citibike-tripdata.csv.zip
+unzip *.zip
+
